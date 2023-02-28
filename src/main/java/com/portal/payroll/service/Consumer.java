58656ec720f7c73ac8fc1527ff7514e2e85d94c7
abhishekVrm1 <abhishek.vrm1@gmail.com>
2023-02-28 15:53:25 +0530

    fix: updated kafka consumer

--- src/main/java/com/portal/payroll/service/Consumer.java
+++ src/main/java/com/portal/payroll/service/Consumer.java
@@ -1,13 +1,27 @@
 package com.portal.payroll.service;
 
+import com.portal.payroll.utility.PayrollInfoException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
+    @Autowired
+    IPayrollService payrollService;
+
     @KafkaListener(topics = "employee_payroll_topic", groupId = "employee_payroll_group")
-    public void receiveMessage(String message){
-        System.out.println("MESSAGE: "+message);
+    public void receiveMessage(String message) throws PayrollInfoException {
+        Double updatedCTC;
+        Integer employeeId;
+        try {
+            String[] rd = message.split(":");
+            updatedCTC = Double.parseDouble(rd[1].strip());
+            employeeId = Integer.parseInt(rd[3].strip());
+            payrollService.updatePayrollInfo(employeeId, updatedCTC);
+        } catch (NumberFormatException ex) {
+            throw new PayrollInfoException("Invalid format message");
+        }
     }
 }
